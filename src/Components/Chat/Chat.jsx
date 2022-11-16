cce544ecec66f3f94489672222b25d4d054bbd9f
JuanFlorez1326 <juanks339@gmail.com>
2022-11-16 10:34:28 -0500

    NEW: Finished frontend design

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Chat.scss';
 import { useState, useEffect } from "react";
 import io from 'socket.io-client'
 
@@ -36,22 +37,21 @@ export const Chat = () => {
     }, [messages])
 
     return (
-        <div className="h-screen bg-slate-400 text-white flex items-center justify-center">
+        <div className="h-screen bg-blue-200 text-white flex items-center justify-center">
             
-            <form onSubmit={handleSubmit} className='bg-blue-500 p-10 rounded w-4/12'>
+            <form onSubmit={handleSubmit} className='card-chat p-10 rounded w-4/12'>
                 <h1 className="text-2x1 font-bold my-2">Room</h1>
                 <input type="text" 
                     value={message} 
                     onChange={(e) => setMessage(e.target.value)}
                     className='border-2 border-zinc-500 text-black w-full'
                 />
-                {/* <button className="bg-blue-500">Send</button> */}
 
                 <ul className="h-80 overflow-y-auto">
                     {
                         messages.map((message, index) => {
                             return (
-                                <li key={index} className={`my-2 p-2 text-sm table rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"}`}>
+                                <li key={index} className={`my-2 p-2 text-sm table rounded-md ${message.from === "Me" ? "bg-zinc-800 ml-auto" : "bg-stone-700"}`}>
                                     <p>{message.from}: {message.body}</p>
                                 </li>
                             )
